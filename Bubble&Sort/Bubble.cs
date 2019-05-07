e8913b5fc521ec0498386142802536df92c72d14
MaxIvanov44 <44645961+MaxIvanov44@users.noreply.github.com>
2019-05-07 20:33:10 +0300

    Add files via upload

---
+++ Bubble&Sort/Bubble.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace bubble_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int[] array = new int[100000];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(-50, 50);//Заполняем случайными числами диапозоном от -100 до 100
+            for (int i = 0; i < array.Length; i++)
+                for (int j = i + 1; j < array.Length; j++)
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+            sw.Stop();
+            Console.WriteLine((sw.ElapsedMilliseconds).ToString());
+            Console.ReadKey(true);
+
+            
+        }
+    }
+}
---
+++ Bubble&Sort/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace bubble_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Stopwatch sw = new Stopwatch();
+            sw.Start();
+            int[] array = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(-50, 50);//Заполняем случайными числами диапозоном от -100 до 100
+             Array.Sort(array);
+            sw.Stop();
+            Console.WriteLine((sw.ElapsedMilliseconds).ToString());
+            Console.ReadKey(true);
+        }
+    }
+}
