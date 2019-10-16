90ef9800bb668b74237cdee735c000ae340f0bcf
Максим <44645961+MaxIvanov44@users.noreply.github.com>
2019-10-16 19:47:14 +0300

    Add files via upload

---
+++ Class creation/Man.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_creation
+{
+    public class Man
+    {
+        public Man(string _name)
+        {
+            Name = _name;
+            isLife = true;
+            Age = (uint)rnd.Next(10, 70);
+            Health = (uint)rnd.Next(100);
+        }
+        private Random rnd = new Random();
+        private string Name;
+        private uint Age;
+        private uint Health;
+        private bool isLife;
+        public void Talk()
+        {
+            int random_talk = rnd.Next(1, 3);
+            string tmp_str = "";
+            switch (random_talk)
+            {
+                case 1:
+                    {
+                        tmp_str = "Привет, меня зовут " + Name + ", рад познакомиться";
+                        break;
+                    }
+                case 2:
+                    {
+                        tmp_str = "Мне " + Age + ". А тебе?";
+                        break;
+                    }
+                case 3:
+                    {
+                        if (Health > 50)
+                            tmp_str = "Да я зводоров как бык!";
+                        else
+                            tmp_str = "Со здоровьем у меня хреново, дожить бы до " + (Age + 10).ToString();
+                        break;
+                    }
+            }
+            System.Console.WriteLine(tmp_str);
+        }
+        public void Go()
+        {
+            if (isLife == true)
+            {
+                if (Health > 40)
+                {
+                    string outString = Name + " мирно прогуливается по городу";
+                    System.Console.WriteLine(outString);
+                }
+                else
+                {
+                    string outString = Name + " болен и не может гулять по городу";
+                    System.Console.WriteLine(outString);
+                }
+            }
+            else
+            {
+                string outString = Name + " не может идти, он умер";
+                System.Console.WriteLine(outString);
+            }
+        }
+        public void Kill()
+        {
+            isLife = false;
+            System.Console.WriteLine(Name + " умер");
+        }
+        public bool IsAlive()
+        {
+            return isLife;
+        }
+    }
+}
---
+++ Class creation/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Class_creation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string user_command = "";
+            bool Infinity = true;
+            Man SomeMan = null;
+            while (Infinity)
+            {
+                System.Console.WriteLine("Пожалуйста, введите команду (help - вывод комманд)\n");
+                user_command = System.Console.ReadLine();
+                switch (user_command)
+                {
+                    case "exit":
+                        {
+                            Infinity = false;
+                            break;
+                        }
+
+                    case "help":
+                        {
+                            System.Console.WriteLine("\nСписок команд:");
+                            System.Console.WriteLine("create : команда создает человека, (экземпляр класса Man)");
+                            System.Console.WriteLine("kill : команда убивает человека");
+                            System.Console.WriteLine("talk : команда застравляет человека говорить (если создан экземпляр класса)");
+                            System.Console.WriteLine("go : команда застравляет человека идти (если создан экземпляр класса)");
+                            System.Console.WriteLine("---\n");
+                            break;
+                        }
+
+                    case "create":
+                        {
+
+                            if (SomeMan != null)
+                            {
+                                SomeMan.Kill();
+                            }
+                            System.Console.WriteLine("Пожалуйста, введите имя создаваемого человека \n");
+                            user_command = System.Console.ReadLine();
+                            SomeMan = new Man(user_command);
+                            System.Console.WriteLine("Человек успешно создан \n");
+                            break;
+                        }
+
+                    case "kill":
+                        {
+                            if (SomeMan != null)
+                            {
+                                SomeMan.Kill();
+                            }
+                            else
+                            {
+                                System.Console.WriteLine("Человек не создан. Вы не можете его убить");
+                            }
+                            break;
+                        }
+
+                    case "talk":
+                        {
+                            if (SomeMan != null)
+                            {
+                                SomeMan.Talk();
+                            }
+                            else
+                            {
+                                System.Console.WriteLine("Человек не создан. Команда не может быть выполнена");
+                            }
+                            break;
+                        }
+
+                    case "go":
+                        {
+                            if (SomeMan != null)
+                            {
+                                SomeMan.Go();
+                            }
+                            else
+                            {
+                                System.Console.WriteLine("Человек не создан. Команда не может быть выполнена");
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            System.Console.WriteLine("Ваша команда не определена, пожалуйста повторите снова\n");
+                      
+                            System.Console.WriteLine("Для завершения программы введите команду exit\n\n");
+                            break;
+                        }
+                }
+            }
+        }
+    }
+
+ 
+}
