5eb7f9acbc15dab2bf9a743f1c046aa9ea130b9c
Максим <44645961+MaxIvanov44@users.noreply.github.com>
2019-10-16 19:37:31 +0300

    Add files via upload

---
+++ method for finding the minimum element of an array/MinEl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace method_for_finding_the_minimum_element_of_an_array
+{
+
+    public class MineL
+    {
+        public static int[] FillArray(int N)
+        {
+            int[] array = new int[N];
+            Random rr = new Random();
+            int c = 1;
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = rr.Next(100);
+                Console.WriteLine(c + "-й элемент массива = " + array[i]);
+                c++;
+            }
+            Console.WriteLine();
+            return array;
+        }
+        public static void MinElem(int[] arr)
+        {
+            //   var min = array.Min();
+            var min = arr[0];
+            foreach (var count in arr)
+            {
+                if (count < min) min = count;
+            }
+            Console.WriteLine("Минимальный = " + min);
+           // return min;
+        }
+    }
+}
---
+++ method for finding the minimum element of an array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace method_for_finding_the_minimum_element_of_an_array
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+        M:
+            Console.Write("Введите размер массива от 1 до 10: ");
+
+            int razm;
+            razm = Convert.ToInt32(Console.ReadLine());
+
+            if (razm > 10 && razm < 1)
+            {
+                Console.Write("Повторите ввод... ");
+                goto M;
+            }
+            else
+            {
+               int[] arr =  MineL.FillArray(razm);
+               
+                MineL.MinElem(arr);
+                
+            }
+            Console.WriteLine();
+            Console.Write("Ждем выход... ");
+            Console.ReadKey();
+        }
+    }
+}
+
+
